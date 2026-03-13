de56fe0391396c6e7c8a16310d221e985af48b65
Juan Oliveira <ruantos@proton.me>
2026-03-13 14:35:35 -0300

    feat: write first node hello_world

--- aula_1/src/server.js
+++ aula_1/src/server.js
@@ -4,11 +4,40 @@
 // forma ESmodules de importação
 import http from 'node:http';
 
+const hostname = '127.0.0.1';
+const port = '3000';
+
+const users = [];
+
 const server = http.createServer((request, response) => {
-    return response.end('Hello World!');
+    const { method, url } = request;
+
+    console.log(method, url)
+    if (method === 'GET' && url === '/users') {
+        
+        return response
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+    }
+    
+    if (method === 'POST' && url === '/users') {
+        users.push(
+            {
+                'id': 1,
+                'name': 'John Doe',
+                'email': 'johndoe@email.com',
+                'age': 31,
+            }
+        );
+        return response.writeHead(201).end()
+    }
+
+    return response.writeHead(404).end();
 })
 
-server.listen(3333);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/users`)
+});
 
 
 
