5ae0db3da7b0d87c63ec8d36a95df335731e2bb8
Juan Oliveira <ruantos@proton.me>
2026-03-19 16:48:57 -0300

    feat: add delete endpoint

--- aula_1/src/database.js
+++ aula_1/src/database.js
@@ -34,5 +34,14 @@ export class Database {
 
   }
 
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex( row => row.id === id )
+    
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
+    }
+  }
+
 
 }
--- aula_1/src/routes.js
+++ aula_1/src/routes.js
@@ -1,3 +1,4 @@
+import { buildRoutePath } from './build_route_path.js';
 import { Database } from './database.js';
 import { randomUUID } from 'node:crypto'; 
 
@@ -7,18 +8,16 @@ const database = new Database;
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users');
   
       return res.end(JSON.stringify(users))
     }
-
   },
-
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { nome, idade, caracteristica } = req.body;
       database.insert(
@@ -30,9 +29,15 @@ export const routes = [
           caracteristica: caracteristica
         });
       return res.writeHead(201).end();
-
     }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
 
-
+      database.delete('users', req.params.id);
+      return res.writeHead(204).end();
+    }
   },
 ]
