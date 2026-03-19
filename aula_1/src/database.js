2725f42fba6da693035290a698532625b24cc5ca
Juan Oliveira <ruantos@proton.me>
2026-03-18 23:08:38 -0300

    feat: make persistant json database

---
+++ aula_1/src/database.js
@@ -0,0 +1,38 @@
+import fs from 'node:fs/promises';
+
+const database_path = new URL('../db_persistant.json', import.meta.url);
+
+export class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(database_path, 'utf8')
+      .then( data => {
+        this.#database = JSON.parse(data); 
+      })
+      .catch( () => {
+        this.#persist();
+      })
+  }
+
+  #persist() {
+    fs.writeFile(database_path, JSON.stringify(this.#database) )
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? [];
+    return data;
+  }
+
+  insert(table, data) {
+    if ( Array.isArray( this.#database[table] ) ) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+    this.#persist();
+
+  }
+
+
+}
--- aula_1/src/server.js
+++ aula_1/src/server.js
@@ -1,33 +1,35 @@
 import http from 'node:http'; 
 import { json } from '../middlewares/json.js';
+import { Database } from './database.js';
 
 const port = 3000;
 const hostname = 'localhost';
 
-const users = []; 
+const db = new Database; 
 
 const server = http.createServer( async(req, res) => {
   
   const { method, url } = req;   
   
-
   await json(req, res);
 
   if (method === 'GET' && url === '/users') {
     return res
-          .end(JSON.stringify(users))
+          .end(JSON.stringify(db.select('users')))
   }
   
   if ( method === 'POST' && url === '/users' ) {
     const { id, nome, idade, caracteristica } = req.body;
     
-    users.push(
+    db.insert(
+      'users',
       {
         id: id,
         nome: nome,
         idade: idade,
         caracteristica: caracteristica
-      });
+      }
+    );
     
     return res.writeHead(201).end();
   }
